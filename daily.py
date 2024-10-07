1856bd5621149bc0acc900409d200415b485d910
Milan <milan_rasool@hotmail.com>
2024-10-07 20:15:47 +0200

    Uppdaterar for-loop i daily.py

--- daily.py
+++ daily.py
@@ -8,7 +8,7 @@ all_lines: list[str] = []
 for filename in files:
     with open(filename, "r") as file:
         for line in file:
-            all_lines.append(line)
+            all_lines.extend([ line.strip() for line in file ])
 
 selected: int = randrange(0, len(all_lines))
 print(all_lines[selected])
